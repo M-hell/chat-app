84de9d01bd58e06ef4465db4957aedc644438037
M-hell <sarkarsamrat16112004@gmail.com>
2024-08-31 00:18:26 +0530

    deploy error

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use('/api',router)
 
 
 //for deploying
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
