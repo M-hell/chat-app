ebcaea2011ea54d74a1988b7f501990dccfce2e3
M-hell <sarkarsamrat16112004@gmail.com>
2024-08-31 15:41:19 +0530

    img render

--- client/src/components/Avatar.jsx
+++ client/src/components/Avatar.jsx
@@ -1,70 +1,70 @@
-import React from 'react'
+import React from 'react';
 import { PiUserCircle } from "react-icons/pi";
 import { useSelector } from 'react-redux';
 
-const Avatar = ({userId,name,imageUrl,width,height}) => {
-    const onlineUser = useSelector(state => state?.user?.onlineUser)
+const Avatar = ({ userId, name, imageUrl, width, height }) => {
+    const onlineUser = useSelector(state => state?.user?.onlineUser);
 
-    let avatarName = ""
+    let avatarName = "";
 
-    if(name){
-      const splitName = name?.split(" ")
+    if (name) {
+        const splitName = name?.split(" ");
 
-      if(splitName.length > 1){
-        avatarName = splitName[0][0]+splitName[1][0]
-      }else{
-        avatarName = splitName[0][0]
-      }
+        if (splitName.length > 1) {
+            avatarName = splitName[0][0] + splitName[1][0];
+        } else {
+            avatarName = splitName[0][0];
+        }
     }
 
     const bgColor = [
-      'bg-slate-200',
-      'bg-teal-200',
-      'bg-red-200',
-      'bg-green-200',
-      'bg-yellow-200',
-      'bg-gray-200',
-      "bg-cyan-200",
-      "bg-sky-200",
-      "bg-blue-200"
-    ]
+        'bg-slate-200',
+        'bg-teal-200',
+        'bg-red-200',
+        'bg-green-200',
+        'bg-yellow-200',
+        'bg-gray-200',
+        "bg-cyan-200",
+        "bg-sky-200",
+        "bg-blue-200"
+    ];
 
-    const randomNumber = Math.floor(Math.random() * 9)
+    const randomNumber = Math.floor(Math.random() * 9);
 
-    const isOnline = onlineUser.includes(userId)
-  return (
-    <div className={`text-slate-800  rounded-full font-bold relative`} style={{width : width+"px", height : height+"px" }}>
-        {
-            imageUrl ? (
-              <div width={width} height={height} className='overflow-hidden rounded-full'>
-                <img
-                    src={imageUrl}
-                    width={width}
-                    height={height}
-                    alt={name}
-                    className='overflow-hidden rounded-full'
-                /></div>
-            ) : (
-                name ? (
-                    <div  style={{width : width+"px", height : height+"px" }} className={`overflow-hidden rounded-full flex justify-center items-center text-lg ${bgColor[randomNumber]}`}>
-                        {avatarName}
+    const isOnline = onlineUser.includes(userId);
+
+    return (
+        <div className={`text-slate-800 rounded-full font-bold relative`} style={{ width: `${width}px`, height: `${height}px` }}>
+            {
+                imageUrl ? (
+                    <div className='overflow-hidden rounded-full' style={{ width: `${width}px`, height: `${height}px` }}>
+                        <img
+                            src={imageUrl}
+                            alt={name}
+                            className='object-cover w-full h-full rounded-full'
+                        />
                     </div>
-                ) :(
-                  <PiUserCircle
-                    size={width}
-                  />
+                ) : (
+                    name ? (
+                        <div style={{ width: `${width}px`, height: `${height}px` }} className={`overflow-hidden rounded-full flex justify-center items-center text-lg ${bgColor[randomNumber]}`}>
+                            {avatarName}
+                        </div>
+                    ) : (
+                        <PiUserCircle
+                            size={width}
+                        />
+                    )
                 )
-            )
-        }
+            }
 
-        {
-          isOnline && (
-            <div className='bg-green-600 p-1 absolute bottom-2 -right-1 z-10 rounded-full'></div>
-          )
-        }
-      
-    </div>
-  )
+            {
+                isOnline && (
+                    <div className='bg-green-600 p-1 absolute bottom-2 -right-1 z-10 rounded-full'></div>
+                )
+            }
+
+        </div>
+    );
 }
 
-export default Avatar
+export default Avatar;
